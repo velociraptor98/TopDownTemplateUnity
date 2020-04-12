83329cd81b52b575917656b2d88a84c9028d4d41
Kunal Singh <kunal1998doon@gmail.com>
2020-04-12 16:20:03 +0530

    Added AI Patrollling logic.

--- Assets/Scripts/EnemyLogic.cs
+++ Assets/Scripts/EnemyLogic.cs
@@ -18,18 +18,59 @@ public class EnemyLogic : MonoBehaviour
     EnemyState state = EnemyState.Idle;
     [SerializeField]
     private Transform destination;
+    [SerializeField]
+    private Transform PatrolStart;
+    [SerializeField]
+    private Transform PatrolEnd;
+    private Vector3 CurrentPosition;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        
+        if(PatrolStart)
+        {
+            CurrentPosition = PatrolStart.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (agent)
+        if(PatrolStart && PatrolEnd)
+        Patrol();
+        /*if (agent && destination)
         {
             agent.SetDestination(destination.position);
         }
+        float distance = Vector3.Distance(destination.transform.position,agent.transform.position);
+        if(distance  < 1.5f)
+        {
+            agent.isStopped = true;
+            agent.velocity = Vector3.zero;
+        }
+        else
+        {
+            agent.isStopped = false;
+        }*/
+    }
+    void Patrol()
+    {
+        if(agent && CurrentPosition != Vector3.zero)
+        {
+            agent.SetDestination(CurrentPosition);
+        }
+        
+            float distance = Vector3.Distance(CurrentPosition, transform.position);
+            if(distance < 1.5f)
+            {
+                if(CurrentPosition == PatrolStart.position)
+                {
+                    CurrentPosition = PatrolEnd.position;
+                }
+                else
+                {
+                    CurrentPosition = PatrolStart.position;
+                }
+            }
+        
     }
 }
